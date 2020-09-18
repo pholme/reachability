80a4872acc492439a0e194b395ce71ccd9861fde
Petter Holme <pttrhlm@gmail.com>
2020-09-18 12:48:03 +0900

    Update reachability.c
    making it a bit more elegant

--- reachability.c
+++ reachability.c
@@ -24,7 +24,7 @@ void merge_reachable (TIMESTEP *t) {
 			for (k = 0; k < g.nu; k++) n[me].rnow[k] |= n[you].rlast[k]; // merging reachable nodes by bit-wise or
 		}
 		for (j = nr = 0; j < g.nu; j++) nr += g.ones[n[me].rlast[j]]; // counting reachable nodes
-		n[me].reach += DIFF(n[me].tlast, t->time) * (double) nr; // weighing by the time since last update
+		n[me].backward += DIFF(n[me].tlast, t->time) * (double) nr; // weighing by the time since last update
 		n[me].tlast = t->time; // remembering the time of this time step
 	}
 }
@@ -34,7 +34,7 @@ void merge_reachable (TIMESTEP *t) {
 
 int main (int argc, char *argv[]) {
 	unsigned int i, j, nr;
-	double d, *forward;
+	double d;
 
 	if (argc != 2) {
 		fprintf(stderr, "usage: reachability [fname]\n");
@@ -55,29 +55,27 @@ int main (int argc, char *argv[]) {
 	for (i = g.nts - 1; i > 0; i--) merge_reachable(ts + i); // go through contacts backward in time
 	for (i = 0; i < g.n; i++) {
 		for (j = nr = 0; j < g.nu; j++) nr += g.ones[n[i].rnow[j]]; // counting reachable nodes
-		n[i].reach += n[i].tlast * nr; // adding contributions between t = 0 and the first contact
+		n[i].backward += n[i].tlast * nr; // adding contributions between t = 0 and the first contact
 	}
-
-	forward = malloc(g.n * sizeof(double));
 
 	// backward reachability
 	for (i = 0; i < g.n; i++) {
-		forward[i] = n[i].reach;
+		n[i].forward = n[i].backward;
 		for (j = 0; j < g.nu; j++) n[i].rnow[j] = n[i].rlast[j] = 0;
 		n[i].rnow[i >> 4] = 1 << (i & 0xf); // initialize the array of reachable nodes (from one node)
 		n[i].tlast = 0;
-		n[i].reach = 0.0;
+		n[i].backward = 0.0;
 	}
 
 	for (i = 0; i < g.nts; i++) merge_reachable(ts + i); // go through contacts forward in time
 	for (i = 0; i < g.n; i++) {
 		for (j = nr = 0; j < g.nu; j++) nr += g.ones[n[i].rnow[j]]; // counting reachable nodes
-		n[i].reach += (g.dur - n[i].tlast) * nr; // adding contributions between t = 0 and the first contact
+		n[i].backward += (g.dur - n[i].tlast) * nr; // adding contributions between t = 0 and the first contact
 	}
 
 	// printing
 	d = (1.0 / g.dur) / g.n;
-	for (i = 0; i < g.n; i++) printf("%u %g %g\n", i, forward[i] * d, n[i].reach * d);
+	for (i = 0; i < g.n; i++) printf("%u %g %g\n", i, n[i].forward * d, n[i].backward * d);
 
 	// free things
 	for (i = 0; i < g.nts; i++) {
@@ -90,7 +88,7 @@ int main (int argc, char *argv[]) {
 		free(n[i].rnow);
 		free(n[i].rlast);
 	}
-	free(ts); free(n); free(forward);
+	free(ts); free(n);
 	 
 	return 0;
 }
